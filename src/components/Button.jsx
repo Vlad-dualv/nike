6df37ed87728d86bf0b57f556eeb4271b66ec4d9
Vlad <vladswork95@gmail.com>
2025-08-13 11:57:17 +0100

    style: Subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,7 @@ export default function Button({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) {
   return (
     <button
@@ -12,7 +13,7 @@ export default function Button({
           backgroundColor
             ? `${backgroundColor} ${textColor} ${borderColor}`
             : "bg-coral-red text-white border-coral-red hover:bg-red-500 focus:bg-red-500 transition-all duration-300"
-        }`}
+        } ${fullWidth && "w-full"}`}
     >
       {label}
       {iconURL && (
