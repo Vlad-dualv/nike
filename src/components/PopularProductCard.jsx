5f5c40667d534732c432fffed15e4453502163ae
Vlad <vladswork95@gmail.com>
2025-08-13 11:17:07 +0100

    style: popular products hover

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -3,7 +3,11 @@ import { star } from "../assets/icons";
 export default function PopularProductCard({ imgURL, name, price }) {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
-      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <img
+        src={imgURL}
+        alt={name}
+        className="w-[280px] h-[280px] hover:scale-110 transition-all duration-300"
+      />
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">
