fd637c4e7c63187d98f69869c4791e32f358ddaf
Vlad <vladswork95@gmail.com>
2025-08-15 21:58:42 +0100

    style: footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,65 @@
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
+
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="logo" width={150} height={46} />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            your perfect size in store. Get rewards.
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full cursor-pointer hover:bg-coral-red transition-all duration-300"
+                key={icon.alt}
+              >
+                <button type="button">
+                  <img src={icon.src} alt={icon.alt} width={24} height={24} />
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap=20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    key={link.name}
+                    className="mt-3 text-white-400 font-montserrat text-base leading-normal cursor-pointer hover:text-slate-grey transition-colors duration-300"
+                  >
+                    <a href={link.name}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copyright icon"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>All rights reserved</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms and conditions</p>
+      </div>
+    </footer>
+  );
 }
