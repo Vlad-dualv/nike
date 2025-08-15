f896d56c1586d119f8680dc9d193e2627bcd6993
Vlad <vladswork95@gmail.com>
2025-08-15 21:31:38 +0100

    fix: subscribe section style

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -3,14 +3,14 @@ import Button from "../components/Button";
 export default function Subscribe() {
   return (
     <section
-      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+      className="max-container flex flex-col items-center gap-10 padding-x padding-y"
       id="contact-us"
     >
-      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+      <h3 className="text-4xl leading-[68px] font-palanquin font-bold text-center">
         Sign Up For<span className="text-coral-red"> Updates </span>&
         Newsletters
       </h3>
-      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-s-slate-gray rounded-full">
+      <div className="w-full max-w-[40%] flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscribe@nike.com" className="input" />
         <Button
           className="flex max-sm:justify-end items-center max-sm:w-full"
